3a33248904f55e80c075c4355117253d459750b2
U-PHYSICSR310\Student <Student@PhysicsR310.kwantlen.ca>
2019-10-28 12:35:39 -0700

    task 3

--- task3/task3.c
+++ task3/task3.c
@@ -5,7 +5,6 @@
 int main()
 {   
 
-/*
 1 0.2
 2 0.3
 3 0.7
@@ -16,9 +15,7 @@ int main()
 8 0.8
 9 0.8
 10 0.1
-*/
 
-/*
 	FILE * input_file;
 	
 
@@ -33,8 +30,8 @@ int main()
 
 	fclose(input_file);
     }
-*/
 
+*/
 
 
 #include <stdio.h>
@@ -42,26 +39,38 @@ int main()
 #include <math.h>
 
 float meanfunction(float *y, int count);
-float std_devfunction(float *y, int count, float mean);
+float std_devfunction(float *y, int count, float mean, FILE *output_file);
 
 int main()
-{   
-	FILE *input_file;
-	input_file = fopen("data.dat","w");
-
+{  
+    char fname[128];
     float sum;
     float mean, std_dev;
     int count = 20;
 	float x[count];
     float y[count];
+    FILE *input_file, *output_file;
 
-    if ((input_file = fopen("data.dat","r")) == NULL)
+    printf("Enter .dat file name: \n"); //Enter file name
+    gets(fname);
+
+	input_file = fopen(fname,"r"); //Opens/reads input file name
+
+    if (input_file == NULL)
     {
          printf("\nError! opening file");
          exit(1);
     }
-	
 
+    output_file = fopen("output.dat","w"); //Opens output file 
+	
+	if (output_file == NULL)
+    {
+         printf("\nError! opening file");
+         exit(1);
+    }
+
+/*
 	for (int i = 0; i < count; ++i)
 	{
 		x[i]=i*1.0/4.0;
@@ -69,21 +78,27 @@ int main()
 
 		printf("\n%f", y[i]);
 	}
-
+*/
 
 	for (int i = 0; i < count; ++i)
-	{
-		fprintf(input_file,"%f %f\n",x[i],y[i]);
+	{   
+        fscanf(input_file, "%f %f",&x[i],&y[i])   //scans input file
+		fprintf(output_file,"%f %f\n",x[i],y[i]); //prints output to output file
         sum+=y[i];
 	}
-	mean = sum/(float)count;
+
+        
+
+	mean = sum/(float)count; 
 
 
-	fclose(input_file);
+    mean  = meanfunction(y,count);  //mean of y data
+	std_dev = std_devfunction(y,count, mean, output_file);  //standard deviation of y data
 
-	mean  = meanfunction(y,count);
-	std_dev = std_devfunction(y,count, mean);
-
+	fclose(input_file);    //closes input file
+ 	fclose(output_file);   //closes output file
+	printf("Data output successful!\n");
+   
 }
 
 float meanfunction(float *y, int count) // Calculates mean of y column of data
@@ -99,7 +114,7 @@ float meanfunction(float *y, int count) // Calculates mean of y column of data
     return mean;
 }
 
-float std_devfunction(float *y, int count, float mean) // Calculates mean of y column of data
+float std_devfunction(float *y, int count, float mean, FILE *output_file) // Calculates mean of y column of data
 {
 
     float numerator = 0;
@@ -111,8 +126,8 @@ float std_devfunction(float *y, int count, float mean) // Calculates mean of y c
     
     float std_dev = sqrt(numerator/(float)count); //calculating standard deviation
 
-    printf("\nMean of y data = %.3f", mean); //mean output
-	printf("\nStandard deviation of y data = %.3f", std_dev); //standard deviation output
+    fprintf(output_file,"\nMean of y data = %.3f", mean); //mean output
+	fprintf(output_file,"\nStandard deviation of y data = %.3f", std_dev); //standard deviation output
 
 	return std_dev;
 }
